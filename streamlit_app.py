d44c6a231c9777118b4165ad8be9019f7d9c403a
Yatharth63 <yatharth24635@iiitd.ac.in>
2025-06-04 22:00:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,16 @@ import yfinance as yf
 import streamlit as st
 import datetime as dt
 
+with st.sidebar:
+    Company = st.text_input("Enter the company ticker")
+    starter = st.text_input("enter start date in format YYYY-MM-DD")
+    ender = st.text_input("enter end date in format YYYY-MM-DD")
+    see_what = st.text_input("what do you want to see in graph, choose one: Open, High, Low, Close, Volume, Dividends, Stock Splits")
 #Title
 st.title("first indicators project")
 st.text("First project importing data from yahoo finance and showing it in graphas and such stuff")
 
-Company = st.text_input("Enter the company ticker")
+
 com = yf.Ticker(Company)
 
 #info
@@ -24,8 +29,8 @@ for key, value in info.items():
 
 st.header("history")
 
-starter = st.text_input("enter start date in format YYYY-MM-DD")
-ender = st.text_input("enter end date in format YYYY-MM-DD")
+
+
 Start_date = dt.datetime.strptime(starter, "%Y-%m-%d")
 end_date = dt.datetime.strptime(ender, "%Y-%m-%d")
 history = com.history(start= Start_date, end = end_date)
@@ -37,10 +42,11 @@ st.table(history)
 #history[Close].plot(title = f"{Company} sStock Price from {starter} to {ender}")
 st.title(f"Visualizing {Company} from {starter} to {ender}")
 
-see_what = st.text_input("what do you want to see, choose one: Open, High, Low, Close, Volume, Dividends, Stock Splits")
+
 
 st.header(f"{see_what}")
 
 st.area_chart(data = history[see_what], x_label="Date", y_label="Price", color="#ff0000", use_container_width=True)
+
 
 
